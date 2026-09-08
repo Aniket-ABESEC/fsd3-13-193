a2207bd118fd08573882f46ba20003567aacd9eb
Aniket Kumar <ak0308002@gmail.com>
2026-09-08 14:49:56 +0530

    add function

--- LabWork/lab3/prg2.js
+++ LabWork/lab3/prg2.js
@@ -1,6 +1,7 @@
 import http from 'http';
 const server = http.createServer((req,res)=>{
-    res.write("<h1>hello Client</h1>");
+    res.write("<h1>Aniket</h1>");
+    res.write("<h2>SIWAN</h2>")
     res.end();
 });
 server.listen(4444,() => console.log("server is running on port 4444...")
