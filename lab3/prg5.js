86de170f371a25b5d870785883bc2b6b5736a523
Aniket Kumar <ak0308002@gmail.com>
2026-09-16 10:51:13 +0530

    Add prg5 and prg6

--- lab3/prg5.js
+++ lab3/prg5.js
@@ -1,12 +1,17 @@
-import http from "http";
+import http from 'http'
 
-const server = http.createServer((req,res)=> {
-    // req method => GET , POST, PUT , DELETE, PATCH
-    console.log("Method:", req.method);
-    console.log("URL;", req.url);
+const server = http.createServer((req,res)=>{
 
-// Broswer can send only GET request to the server
-// POST/PUT/PATCH/DELETE => Can be checked  by7 api tester
-// API Tester =>postman, echo api, thunder client   
-})
+    // req method -> GET , POST , PUT , DELETE, PATCH
+    console.log('Method: req.method')
+    console.log("URL:", req.url);
 
+    //Browser can send only requests to the server
+    // POST/PATCH/PUT/DELETE -> can be checked by api tester
+    //API Tester -> postman, echo api, thunder client
+    if (req.url==="/"){
+        res.setHeader("content-type","text/html");
+        res.end("<h1>hello server</h1>");
+    }
+});
+server.listen(5000,() =>console.log("prg5 is running"));
